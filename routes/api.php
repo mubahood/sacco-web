cbe1e577b0646460077c4730d7aa64ccc6def900
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-09-12 19:59:09 +0300

    Romina test

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::POST("users/register", [ApiAuthController::class, "register"]);
 Route::POST("users/login", [ApiAuthController::class, "login"]);
+Route::get("test", function () {
+    die("Romina test");
+});
 
 Route::group(['middleware' => 'api'], function ($router) {
     Route::get("users/me", [ApiAuthController::class, 'me']);
